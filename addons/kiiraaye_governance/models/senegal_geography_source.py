39db535114eb6c9c3706648ca06dc691d01cee28
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-17 17:33:40 +0000

    feat(kiiraaye): add GalsenAPI geography source option

---
+++ addons/kiiraaye_governance/models/senegal_geography_source.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class ResCountrySenegalGeographySource(models.Model):
+    _inherit = "res.country"
+
+    kiiraaye_geo_source = fields.Selection(
+        selection_add=[
+            ("galsenapi", "GalsenAPI (Sénégal)"),
+        ]
+    )
