88729ff2ecb192d967420b71fc1d1a0939c8a4ad
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-17 16:25:31 +0000

    feat(kiiraaye): register country geography validation

--- addons/kiiraaye_governance/models/__init__.py
+++ addons/kiiraaye_governance/models/__init__.py
@@ -1,5 +1,6 @@
 from . import geographie
 from . import country_geography
+from . import country_geography_validation
 from . import section_geography_config
 from . import partisan
 from . import position
