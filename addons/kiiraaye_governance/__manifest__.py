7e754f34ee56a3b66a311c3069e57f78caa74c4a
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-20 01:18:58 +0000

    [KIIRAAY] Déclarer Ralliement et hiérarchie

--- addons/kiiraaye_governance/__manifest__.py
+++ addons/kiiraaye_governance/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Kiiraaye Gouvernance",
-    "version": "19.0.2.22.48",
+    "version": "19.0.2.22.49",
     "category": "Organization",
     "summary": "Référentiel géographique et gestion des sections/coordinations Kiiraaye",
     "description": "Référentiel pays et subdivisions administratives, sections/coordinations, membres, bureaux, cartes membres, attributions de postes et contrôle d'accès hiérarchique.",
@@ -39,6 +39,8 @@
         "views/report_member_card_modern.xml",
         "views/dashboard_views.xml",
         "views/demo_data_wizard_views.xml",
+        "views/ralliement_views.xml",
+        "views/section_hierarchy_views.xml",
         "views/menus.xml"
     ],
     "assets": {
