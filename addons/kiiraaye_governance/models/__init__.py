6ad2aa3fcae5bed50a0ce7182d7efa6ae72ae38f
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-20 13:54:08 +0000

    Retrait du wizard de hiérarchie

--- addons/kiiraaye_governance/models/__init__.py
+++ addons/kiiraaye_governance/models/__init__.py
@@ -24,5 +24,4 @@ from . import dashboard
 from . import demo_data_wizard
 from . import ralliement
 from . import ralliement_add_member_wizard
-from . import section_hierarchy_wizard
 from . import resend_mail
