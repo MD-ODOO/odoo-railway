dd995590db05b93a9cf68beac4824364dfbe3801
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-21 13:17:51 +0000

    Ajout du vrai module muk_web_appsbar Odoo 19

--- addons/muk_web_appsbar/__manifest__.py
+++ addons/muk_web_appsbar/__manifest__.py
@@ -1,76 +1,65 @@
 {
-    'name': 'MuK AppsBar',
-    'summary': 'Adds a sidebar to the main screen',
-    'description': """
-        This module adds a sidebar to the main screen. The sidebar has a list
-        of all installed apps similar to the home menu to ease navigation.
+    "name": "MuK AppsBar",
+    "summary": "Adds a dynamic sidebar to the main screen",
+    "description": """
+        MuK AppsBar for Odoo 19.
+        The applications sidebar appears on hover and expands smoothly.
+        The colors are configurable through CSS variables.
     """,
-    'version': '19.0.1.1.12',
-    'category': 'Tools/UI',
-    'license': 'LGPL-3',
-    'author': 'MuK IT',
-    'website': 'http://www.mukit.at',
-    'live_test_url': 'https://youtu.be/kmu69REqKDU',
-    'contributors': [
-        'Mathias Markl <mathias.markl@mukit.at>',
+    "version": "19.0.1.1.12",
+    "category": "Tools/UI",
+    "license": "LGPL-3",
+    "author": "MuK IT",
+    "website": "http://www.mukit.at",
+    "contributors": ["Mathias Markl <mathias.markl@mukit.at>"],
+    "depends": ["base_setup", "web"],
+    "data": [
+        "templates/webclient.xml",
+        "views/res_users.xml",
+        "views/res_config_settings.xml",
     ],
-    'depends': [
-        'base_setup',
-        'web',
-    ],
-    'data': [
-        'templates/webclient.xml',
-        'views/res_users.xml',
-        'views/res_config_settings.xml',
-    ],
-    'assets': {
-        'web._assets_primary_variables': [
-            'muk_web_appsbar/static/src/scss/variables.scss',
+    "assets": {
+        "web._assets_primary_variables": [
+            "muk_web_appsbar/static/src/scss/variables.scss",
         ],
-        'web._assets_backend_helpers': [
-            'muk_web_appsbar/static/src/scss/mixins.scss',
+        "web._assets_backend_helpers": [
+            "muk_web_appsbar/static/src/scss/mixins.scss",
         ],
-        'web.assets_web_dark': [
+        "web.assets_web_dark": [
             (
-                'after',
-                'muk_web_appsbar/static/src/scss/variables.scss',
-                'muk_web_appsbar/static/src/scss/variables.dark.scss',
+                "after",
+                "muk_web_appsbar/static/src/scss/variables.scss",
+                "muk_web_appsbar/static/src/scss/variables.dark.scss",
             ),
         ],
-        'web.assets_backend': [
+        "web.assets_backend": [
             (
-                'after',
-                'web/static/src/webclient/webclient.js',
-                'muk_web_appsbar/static/src/webclient/webclient.js',
+                "after",
+                "web/static/src/webclient/webclient.js",
+                "muk_web_appsbar/static/src/webclient/webclient.js",
             ),
             (
-                'after',
-                'web/static/src/webclient/webclient.xml',
-                'muk_web_appsbar/static/src/webclient/webclient.xml',
+                "after",
+                "web/static/src/webclient/webclient.xml",
+                "muk_web_appsbar/static/src/webclient/webclient.xml",
             ),
             (
-                'after',
-                'web/static/src/webclient/webclient.js',
-                'muk_web_appsbar/static/src/webclient/menus/app_menu_service.js',
+                "after",
+                "web/static/src/webclient/webclient.js",
+                "muk_web_appsbar/static/src/webclient/menus/app_menu_service.js",
             ),
             (
-                'after',
-                'web/static/src/webclient/webclient.js',
-                'muk_web_appsbar/static/src/webclient/appsbar/appsbar.js',
+                "after",
+                "web/static/src/webclient/webclient.js",
+                "muk_web_appsbar/static/src/webclient/appsbar/appsbar.js",
             ),
-            'muk_web_appsbar/static/src/webclient/webclient.scss',
-            'muk_web_appsbar/static/src/webclient/appsbar/appsbar.xml',
-            'muk_web_appsbar/static/src/webclient/appsbar/appsbar.scss',
-        ],
-        'web.assets_unit_tests': [
-            'muk_web_appsbar/static/tests/**/*',
+            "muk_web_appsbar/static/src/webclient/webclient.scss",
+            "muk_web_appsbar/static/src/webclient/appsbar/appsbar.xml",
+            "muk_web_appsbar/static/src/webclient/appsbar/appsbar.scss",
         ],
     },
-    'images': [
-        'static/description/banner.png',
-    ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    'post_init_hook': '_setup_module',
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "post_init_hook": "_setup_module",
 }
