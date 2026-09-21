de8623190a9c26ab9e42ee2fb9349b682f566f3d
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-21 13:17:48 +0000

    Ajout du vrai module muk_web_appsbar Odoo 19

--- addons/muk_web_appsbar/models/res_config_settings.py
+++ addons/muk_web_appsbar/models/res_config_settings.py
@@ -1,18 +1,10 @@
-from __future__ import annotations
-
 from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    """Expose the company appbar image in the general settings."""
-
-    _inherit = 'res.config.settings'
-
-    # ----------------------------------------------------------
-    # Fields
-    # ----------------------------------------------------------
+    _inherit = "res.config.settings"
 
     appbar_image = fields.Binary(
-        related='company_id.appbar_image',
+        related="company_id.appbar_image",
         readonly=False,
     )
