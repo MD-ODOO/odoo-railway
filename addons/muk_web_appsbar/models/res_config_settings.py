fbd977a47fd99cd4b7678001a83b4863cb0ddc5c
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-21 15:59:13 +0000

    AppsBar: exposer la couleur dans les paramètres

--- addons/muk_web_appsbar/models/res_config_settings.py
+++ addons/muk_web_appsbar/models/res_config_settings.py
@@ -8,3 +8,8 @@ class ResConfigSettings(models.TransientModel):
         related="company_id.appbar_image",
         readonly=False,
     )
+
+    appbar_background_color = fields.Char(
+        related="company_id.appbar_background_color",
+        readonly=False,
+    )
