72b97f3a163de5b227b66f8db93dc26eccba9eed
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-21 16:10:15 +0000

    AppsBar: créer automatiquement la colonne SQL manquante

--- addons/muk_web_appsbar/models/res_company.py
+++ addons/muk_web_appsbar/models/res_company.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.tools.sql import column_exists, create_column
 
 
 class ResCompany(models.Model):
@@ -14,3 +15,15 @@ class ResCompany(models.Model):
         default="#172033",
         help="Couleur libre utilisée comme couleur de fond principale de la barre des applications.",
     )
+
+    def _auto_init(self):
+        # Répare automatiquement les bases où le champ Python existe déjà
+        # mais où la colonne SQL n'a pas encore été créée.
+        if not column_exists(self.env.cr, "res_company", "appbar_background_color"):
+            create_column(
+                self.env.cr,
+                "res_company",
+                "appbar_background_color",
+                "varchar",
+            )
+        return super()._auto_init()
