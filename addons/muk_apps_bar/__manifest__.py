61c3ddfff66506d39c758749fc5dd077d7389a9d
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-21 13:11:00 +0000

    Ajout barre applications dynamique au survol

---
+++ addons/muk_apps_bar/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "MuK Apps Bar - Kiiraaye",
+    "version": "19.0.1.0.0",
+    "category": "Web",
+    "summary": "Barre d'applications dynamique au survol",
+    "description": "Barre latérale d'applications inspirée de MuK Apps Bar, avec ouverture au survol et couleurs personnalisables par variables CSS.",
+    "author": "Kiiraaye",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "muk_apps_bar/static/src/js/apps_bar.js",
+            "muk_apps_bar/static/src/scss/apps_bar.scss"
+        ]
+    },
+    "installable": true,
+    "application": false
+}
