24ca16c1a321cad022a03a833360c2d327541c5f
Mansour Diop <mnsourdiop@gmail.com>
2026-09-25 17:17:08 +0000

    demo

--- addons/kiiraaye_parrainage/__manifest__.py
+++ addons/kiiraaye_parrainage/__manifest__.py
@@ -4,7 +4,12 @@
     "category": "Organization",
     "summary": "Gestion hiérarchique des listes de parrainage nationales et diaspora",
     "depends": ["base", "mail", "kiiraaye_governance", "web"],
-    "data": ["security/groups.xml","security/ir.model.access.csv","security/parrainage_rules.xml","data/sequence.xml","data/demo_data.xml","data/national_demo.xml","views/parrainage_views.xml","views/import_views.xml","views/dashboard_views.xml","views/menus.xml"],
+    "data": ["security/groups.xml","security/ir.model.access.csv",
+             "security/parrainage_rules.xml","data/sequence.xml",
+             "data/demo_data.xml",
+            #  "data/national_demo.xml",
+             "views/parrainage_views.xml",
+             "views/import_views.xml","views/dashboard_views.xml","views/menus.xml"],
     "assets": {
         "web.assets_backend": [
             "kiiraaye_parrainage/static/src/js/parrainage_dashboard.js",
