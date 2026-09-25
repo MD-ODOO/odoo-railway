8c61e363fdb6165150eb8f5c86f3bbc3bb9baafb
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-25 16:17:27 +0000

    Configure OWL dashboard and demo data

--- addons/kiiraaye_parrainage/__manifest__.py
+++ addons/kiiraaye_parrainage/__manifest__.py
@@ -1,10 +1,17 @@
 {
     "name": "Kiiraaye - Gestion des parrainages",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "category": "Organization",
     "summary": "Gestion hiérarchique des listes de parrainage nationales et diaspora",
-    "depends": ["base", "mail", "kiiraaye_governance"],
-    "data": ["security/groups.xml","security/ir.model.access.csv","security/parrainage_rules.xml","data/sequence.xml","views/parrainage_views.xml","views/import_views.xml","views/dashboard_views.xml","views/menus.xml"],
+    "depends": ["base", "mail", "kiiraaye_governance", "web"],
+    "data": ["security/groups.xml","security/ir.model.access.csv","security/parrainage_rules.xml","data/sequence.xml","data/demo_data.xml","views/parrainage_views.xml","views/import_views.xml","views/dashboard_views.xml","views/menus.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "kiiraaye_parrainage/static/src/js/parrainage_dashboard.js",
+            "kiiraaye_parrainage/static/src/xml/parrainage_dashboard.xml",
+            "kiiraaye_parrainage/static/src/scss/parrainage_dashboard.scss"
+        ]
+    },
     "installable": True,
     "application": True,
     "license": "LGPL-3"
