f6ace85371a709ded3346cfd4123ccc56dda9197
MAJNOUNOU KHAWSOU <104442926+MD-ODOO@users.noreply.github.com>
2026-09-26 13:17:42 +0000

    fix: start Odoo with Railway PostgreSQL settings

--- scripts/kiiraaye-entrypoint.sh
+++ scripts/kiiraaye-entrypoint.sh
@@ -17,4 +17,9 @@ if [ "$#" -eq 0 ]; then
     set -- odoo
 fi
 
+if [ "$1" = "odoo" ] || [ "$1" = "odoo-bin" ]; then
+    shift
+    exec su -s /bin/bash odoo -c "odoo --db_host=\"$DB_HOST\" --db_port=\"$DB_PORT\" --db_user=\"$DB_USER\" --db_password=\"$DB_PASSWORD\" $*"
+fi
+
 exec "$@"
